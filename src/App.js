0f267eca0f34c577e2d61172b0051841d435dd2f
mehdinajeddine <mehdi.najeddine@void.fr>
2022-01-13 20:17:00 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Convert from "./components/Convert";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/pro-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Taux de change au Maroc</h1>
+        <p>Pour convertir le dirham Marocain dans toutes les devises.</p>
       </header>
+      <main>
+        <Convert />
+      </main>
+      <footer>
+        <hr />
+        <p className="small">
+          taux-de-change.ma © 2022
+          <br />
+          L'utilisation de ce site est gratuite et réservée à un usage
+          strictement personnel.
+          <br />
+          Created with{" "}
+          <FontAwesomeIcon
+            icon={faHeart}
+            onClick={(e) => {
+              handleSubmit("res2source");
+            }}
+          />{" "}
+          in Morocco
+        </p>
+      </footer>
     </div>
   );
 }
---
+++ src/components/Convert.js
@@ -0,0 +1,147 @@
+import { useState, useEffect } from "react";
+import rates from "../rates.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowAltDown, faArrowAltUp } from "@fortawesome/pro-solid-svg-icons";
+import axios from "axios";
+import { Switch } from "@mui/material";
+
+const Convert = () => {
+  const [source, setSource] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const [csource, setCsource] = useState("EUR");
+  const [cresult, setCresult] = useState("USD");
+  const [data, setData] = useState([]);
+  const [type, setType] = useState(false);
+
+  const handleSubmit = (direction) => {
+    const ratesResult = data.filter((val) => {
+      return val.libDevise == cresult ? true : false;
+    });
+    const ratesSource = data.filter((val) => {
+      return val.libDevise == csource ? true : false;
+    });
+    let rs = 1;
+    let rr = 1;
+
+    if (!type) {
+      rs = ratesSource.length > 0 ? ratesSource[0].achatClientele : 1;
+      rr = ratesResult.length > 0 ? ratesResult[0].achatClientele : 1;
+    } else {
+      rs = ratesSource.length > 0 ? ratesSource[0].venteClientele : 1;
+      rr = ratesResult.length > 0 ? ratesResult[0].venteClientele : 1;
+    }
+
+    if (direction === "source2res") {
+      setResult((source * rs) / rr);
+    } else {
+      setSource((result * rr) / rs);
+    }
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      const params = { libDevise: "", date: "2022-01-12T21:44:09.921Z" };
+      const headers = {
+        headers: {
+          "Ocp-Apim-Subscription-Key": "79fc75e4ae334e388a444d060670388d",
+        },
+      };
+      const data = await axios.get(
+        "https://api.centralbankofmorocco.ma/cours/Version1/api/CoursBBE?libDevise=&date=2022-01-12T21:44:09.921Z",
+
+        headers
+      );
+      setData(data.data);
+      console.log(data.data);
+    };
+    getData();
+    setSource(1);
+    setCsource("MAD");
+  }, []);
+
+  const currencies = Object.keys(rates);
+  const currenciesList = data.map((key, index) => {
+    return (
+      <option value={key.libDevise} key={index}>
+        {key.libDevise}
+      </option>
+    );
+  });
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit("source2res");
+      }}
+    >
+      <div>
+        <div className="type">
+          <div>
+            <span>Achat</span>
+            <Switch
+              onClick={(e) => {
+                setType(!type);
+              }}
+            ></Switch>
+            <span>Vente</span>
+          </div>
+          <div>
+            <p className="medium">
+              Achat repésente le coût d'achat d'une devise. Autrement dit, vous
+              changez vos dirhams en devises.
+            </p>
+            <p className="medium">
+              Vente repésente le coût de vente d'une devise. Autrement dit, vous
+              changez vos devises en dirhams.
+            </p>
+          </div>
+        </div>
+        <input
+          type="number"
+          value={source}
+          onChange={(e) => setSource(e.target.value)}
+        />
+        <select value={csource} onChange={(e) => setCsource(e.target.value)}>
+          {currenciesList}
+          <option value="MAD" key="MAD">
+            MAD
+          </option>
+        </select>
+      </div>
+      <div>
+        <button className="btn-convert">Convertir</button>{" "}
+        <FontAwesomeIcon
+          size="2x"
+          icon={faArrowAltDown}
+          onClick={(e) => {
+            handleSubmit("source2res");
+          }}
+        />
+        <FontAwesomeIcon
+          size="2x"
+          icon={faArrowAltUp}
+          onClick={(e) => {
+            handleSubmit("res2source");
+          }}
+        />
+      </div>
+      <div>
+        <input
+          value={result}
+          type="number"
+          onChange={(e) => setResult(e.target.value)}
+        />
+        <select value={cresult} onChange={(e) => setCresult(e.target.value)}>
+          {currenciesList}
+          <option value="MAD" key="MAD">
+            MAD
+          </option>
+        </select>
+      </div>
+    </form>
+  );
+};
+
+export default Convert;
